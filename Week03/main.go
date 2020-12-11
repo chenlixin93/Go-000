7c31cdb63e857c064d2e0cdafe22f841314da727
jasper <jasper.chen@sain3.com>
2020-12-11 20:53:12 +0800

    week03

--- Week03/main.go
+++ Week03/main.go
@@ -13,39 +13,43 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-var done = make(chan int)
-
 func main() {
-	ctxContext, cancel := context.WithCancel(context.Background())
-	group, ctx := errgroup.WithContext(ctxContext)
-	group.Go(func() error {
-		mux := http.NewServeMux()
-		mux.HandleFunc("/close", func(writer http.ResponseWriter, request *http.Request) {
+	done := make(chan int)
+	eg, ctx := errgroup.WithContext(context.Background())
+	eg.Go(func() error {
+		handler := http.NewServeMux()
+		handler.HandleFunc("/close", func(writer http.ResponseWriter, request *http.Request) {
 			done <- 1
 		})
-		s := NewServer(":8080", ctx, mux)
-		go func() {
-			err := s.Start()
-			if err != nil {
-				fmt.Println(err)
-			}
-		}()
-		select {
-		case <-done:
-			cancel()
-			fmt.Println("协程1：访问/close路由，触发信号关闭1")
-			return s.Stop()
-		case <-ctx.Done():
-			fmt.Println("协程1：来自信号关闭2")
-			return errors.New("协程1：【通知】信号关闭")
+		s := &http.Server{
+			Addr:    ":8080",
+			Handler: handler,
 		}
+
+		httpEg, httpCtx := errgroup.WithContext(context.Background())
+		httpEg.Go(func() error {
+			return s.ListenAndServe()
+		})
+		httpEg.Go(func() error {
+			select {
+				case <-done:
+					fmt.Println("协程1：访问/close路由，触发信号关闭1")
+				case <-ctx.Done():
+					fmt.Println("协程1：来自信号关闭2")
+				case <-httpCtx.Done():
+					fmt.Println("http server error")
+					return errors.New("http server error")
+			}
+			timeoutCtx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+			return s.Shutdown(timeoutCtx)
+		})
+		return httpEg.Wait()
 	})
-	group.Go(func() error {
+	eg.Go(func() error {
 		quit := make(chan os.Signal)
 		signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
 		select {
 		case <-quit:
-			cancel()
 			fmt.Println("协程2：监听系统信号量，触发信号关闭2")
 			return errors.New("协程2：信号关闭2")
 		case <-ctx.Done():
@@ -53,36 +57,7 @@ func main() {
 			return errors.New("协程2：【通知】http服务关闭")
 		}
 	})
-
 	fmt.Println("开始捕捉err")
-	err := group.Wait()
+	err := eg.Wait()
 	fmt.Println("=======", err)
-}
-
-//http服务
-type httpServer struct {
-	s *http.Server
-	handler http.Handler
-	cxt context.Context
-}
-
-func NewServer(address string, ctxContext context.Context, mux http.Handler) *httpServer {
-	h := &httpServer{cxt: ctxContext}
-	h.s = &http.Server{
-		Addr: address,
-		WriteTimeout: time.Second * 3,
-		Handler: mux,
-	}
-	return h
-}
-
-func (h *httpServer) Start() error {
-	fmt.Println("httpServer start")
-	return h.s.ListenAndServe()
-}
-
-func (h *httpServer) Stop() error {
-	_ = h.s.Shutdown(h.cxt)
-	fmt.Println("httpServer结束1")
-	return fmt.Errorf("httpServer结束2")
 }
